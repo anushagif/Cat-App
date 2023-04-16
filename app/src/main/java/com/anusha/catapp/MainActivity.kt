c4a837bebcd03bdcba3c7d7eda53ca1b0099324f
Anusha c <canusha215@gmail.com>
2023-04-16 16:08:02 +0530

    fix:jitpack failed

--- app/src/main/java/com/anusha/catapp/MainActivity.kt
+++ app/src/main/java/com/anusha/catapp/MainActivity.kt
@@ -10,14 +10,14 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.anusha.cat_info.TextName
 import com.anusha.catapp.ui.theme.CatAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-
+         TextName()
         }
     }
 }
